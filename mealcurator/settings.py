a68139f69b08a30a23a997a21c8216e952ee1964
jtweeder <31365056+jtweeder@users.noreply.github.com>
2022-01-16 17:36:49 -0600

    azure build

--- mealcurator/settings.py
+++ mealcurator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h@f8ah7p6j_ati9j1im$2977@v4b7*)uc(#)^csb+4m84z@j)l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'meals',
 ]
 
 MIDDLEWARE = [
