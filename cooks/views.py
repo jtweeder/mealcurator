24324526d2057a44e5cf8d88473d61b3f70263cf
Jefferson Tweed <31365056+jtweeder@users.noreply.github.com>
2022-02-25 22:43:37 -0600

    Update views.py

--- cooks/views.py
+++ cooks/views.py
@@ -1,4 +1,3 @@
-from unittest.mock import NonCallableMagicMock
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
