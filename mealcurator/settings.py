4c4eeb577a037993b556c06a7c530c701dc2e08b
jtweeder <31365056+jtweeder@users.noreply.github.com>
2022-02-26 14:37:42 -0600

    rec cards and one rating per

--- mealcurator/settings.py
+++ mealcurator/settings.py
@@ -125,12 +125,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'welcome'
 LOGOUT_REDIRECT_URL = 'login'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "dev_static")]
