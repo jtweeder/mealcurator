b546e74d82e3f5c7b218fe8243ad2994674393d5
jtweeder <jtweeder@gmail.com>
2023-07-12 19:05:05 -0500

    final touches to launch blog feature

--- stewpot/urls.py
+++ stewpot/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', share_views.save_share, name='save-shared'),
     path('view/<int:share_id>', share_views.view_share, name='view-shared'),
     path('post/<int:post_id>', share_views.view_posting, name='view-posting'),
+    path('post/', share_views.home_postings, name='home-postings'),
+
     ]
